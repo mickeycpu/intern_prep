4ac6efa885867729e636aa2e08b0e4b505a35379
张添宇 <15871427062@163.com>
2026-04-05 18:07:49 +0800

    Day3: 字典推导式练习

--- hello.py
+++ hello.py
@@ -1,13 +1,13 @@
-#  题目一
-# s = [i for i  in range(1,21) if i % 2 != 0]
-# print(s)
+#  题目一 --->生成一个字典，键是1到10，值是该数字是否为偶数
+s = {i:i % 2 == 0 for i in range(1,11) }
+print(s)
 
-#  题目二
-# words = ["hello", "world", "python", "flask"]
-# words2 = [s.upper() for s in words ]
-# print(words2)
+#  题目二 --->把下面的温度（摄氏度）转换成华氏度，存成字典
+cities = {"北京": 5, "上海": 12, "武汉": 10, "广州": 22}
+cities2 = {city:temp * 1.8 + 32 for city,temp in cities.items()}
+print(cities2)
 
-#  题目三
-scores = [72, 88, 95, 43, 81, 67, 90]
-scores2 = [s + 5 for s in scores  if s >= 80]
-print(scores2)
+#  题目三 --->从分数从学生成绩字典中，只保留90分以上的学生，并给每人加一个标签"优秀"列表中，只保留80分以上的，并给每人加5分
+students = {"张三": 85, "李四": 92, "王五": 97, "赵六": 78, "钱七": 91}
+students2 = {name:f"{score}-优秀" for name, score in students.items() if score >= 90}
+print(students2)
