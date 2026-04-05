d1fbc75e0e89ef5f38a375b4fdf4a469ad2c50a1
张添宇 <15871427062@163.com>
2026-04-05 17:12:34 +0800

    Day1: 第一个Python程序

---
+++ hello.py
@@ -0,0 +1,10 @@
+# 这是你实习备战的第一个程序
+name = "mickeycpu"
+school = ""
+major = "软件技术"
+goal = "3个月后拿到Python实习offer"
+
+print(f"我叫{name}")
+print(f"来自{school}{major}")
+print(f"我的目标是：{goal}")
+print("今天是备战第1天，加油！")
